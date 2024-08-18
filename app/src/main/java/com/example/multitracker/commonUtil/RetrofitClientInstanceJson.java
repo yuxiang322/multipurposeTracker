26be625455617f004303c334d996a69c3d09f850
yuxiang322 <rlptraum@gmail.com>
2024-08-19 07:40:04 +0800

    2nd commit 1. Login 2. Register

--- app/src/main/java/com/example/multitracker/commonUtil/RetrofitClientInstanceJson.java
+++ app/src/main/java/com/example/multitracker/commonUtil/RetrofitClientInstanceJson.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientInstanceJson {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://192.168.0.11:8080/"; 
+    private static final String BASE_URL = "http://192.168.0.11:8080/";
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
