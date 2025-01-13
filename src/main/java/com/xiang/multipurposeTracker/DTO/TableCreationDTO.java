3b9bd8786db78b6f9de6441049775e9c802e4de8
yuxiang322 <rlptraum@gmail.com>
2025-01-13 16:42:04 +0800

    8th commit 1. Additional SMTP changes

---
+++ src/main/java/com/xiang/multipurposeTracker/DTO/TableCreationDTO.java
@@ -0,0 +1,31 @@
+package com.xiang.multipurposeTracker.DTO;
+
+import java.util.List;
+
+public class TableCreationDTO {
+    private TemplateTablesDTO templateTables;
+    private List<HeaderDetailsDTO> headerDetailsList;
+
+    public TableCreationDTO() {
+
+    }
+    public TableCreationDTO(TemplateTablesDTO templateTables, List<HeaderDetailsDTO> headerDetailsList){
+        this.templateTables = templateTables;
+        this.headerDetailsList = headerDetailsList;
+    }
+    public TemplateTablesDTO getTemplateTables() {
+        return templateTables;
+    }
+
+    public void setTemplateTables(TemplateTablesDTO templateTables) {
+        this.templateTables = templateTables;
+    }
+
+    public List<HeaderDetailsDTO> getHeaderDetailsList() {
+        return headerDetailsList;
+    }
+
+    public void setHeaderDetailsList(List<HeaderDetailsDTO> headerDetailsList) {
+        this.headerDetailsList = headerDetailsList;
+    }
+}
