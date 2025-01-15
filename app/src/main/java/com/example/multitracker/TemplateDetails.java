1a2e21f28a05790ee9237b5def8e6e7efb5d8353
yuxiang322 <rlptraum@gmail.com>
2025-01-15 16:40:19 +0800

    1. Populate Template Details 2. Delete tables
    -> once done work on caching for all modules

--- app/src/main/java/com/example/multitracker/TemplateDetails.java
+++ app/src/main/java/com/example/multitracker/TemplateDetails.java
@@ -9,17 +9,24 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.multitracker.dto.TemplateDTO;
+import com.example.multitracker.dto.TemplateTablesDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TemplateDetails extends AppCompatActivity {
 
     private TemplateDTO templateDTOnotification;
     private int templateID;
 
+    private Boolean enableDelete;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.template_detail);
 
+        enableDelete = false;
         // Retrieval of template table details
         setupTemplateIntent();
         // Set up button view Save, Cancel
@@ -34,34 +41,42 @@ public class TemplateDetails extends AppCompatActivity {
         addTableButton();
 
     }
+
     @Override
     protected void onResume() {
         super.onResume();
     }
 
-    private void initialUIState(){
+    private void initialUIState() {
         Button saveButton = findViewById(R.id.addTables);
         Button cancelButton = findViewById(R.id.cancelSaveTemplate);
         Button addTable = findViewById(R.id.addTable);
+        Button deleteTable = findViewById(R.id.removeTable);
         saveButton.setVisibility(View.GONE);
         cancelButton.setVisibility(View.GONE);
         addTable.setVisibility(View.GONE);
+        deleteTable.setVisibility(View.GONE);
     }
-    private void enableButtons(){
+
+    private void enableButtons() {
         Button saveButton = findViewById(R.id.addTables);
         Button cancelButton = findViewById(R.id.cancelSaveTemplate);
         Button addTable = findViewById(R.id.addTable);
+        Button deleteTable = findViewById(R.id.removeTable);
         saveButton.setVisibility(View.VISIBLE);
         cancelButton.setVisibility(View.VISIBLE);
         addTable.setVisibility(View.VISIBLE);
+        deleteTable.setVisibility(View.VISIBLE);
     }
-    private void stateEditTemplate(Boolean editTextFlag){
+
+    private void stateEditTemplate(Boolean editTextFlag) {
         EditText templateName = findViewById(R.id.templateName);
         EditText templateDescription = findViewById(R.id.templateDescription);
         templateName.setEnabled(editTextFlag);
         templateDescription.setEnabled(editTextFlag);
     }
-    private void setupEditButtonListener(){
+
+    private void setupEditButtonListener() {
         Button editButton = findViewById(R.id.editTemplate);
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,7 +89,7 @@ public class TemplateDetails extends AppCompatActivity {
     }
 
     // Add table button
-    private void addTableButton(){
+    private void addTableButton() {
         Button addTable = findViewById(R.id.addTable);
 
         addTable.setOnClickListener(new View.OnClickListener() {
@@ -88,14 +103,27 @@ public class TemplateDetails extends AppCompatActivity {
         });
     }
 
-    private void saveCancelButtonListener(){
+    private void notificationButtonListener() {
+        Button notiButton = findViewById(R.id.topRightNotificationButton);
+        notiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TemplateDetails.this, NotificationSetting.class);
+                intent.putExtra("templateObject", templateDTOnotification);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void saveCancelButtonListener() {
         Button saveButton = findViewById(R.id.addTables);
         Button cancelButton = findViewById(R.id.cancelSaveTemplate);
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // ??????
+                // Only saves Template information.
+                // delete(prompt) and add Table from + button are permanent.
             }
         });
         cancelButton.setOnClickListener(new View.OnClickListener() {
@@ -105,30 +133,43 @@ public class TemplateDetails extends AppCompatActivity {
                 initialUIState();
                 stateEditTemplate(false);
                 // Reload text if there are changes made. Populate
-                setupTemplateIntent(); //maybe
-            }
-        });
-    }
-    private void notificationButtonListener(){
-        Button notiButton = findViewById(R.id.topRightNotificationButton);
-        notiButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(TemplateDetails.this,NotificationSetting.class);
-                intent.putExtra("templateObject", templateDTOnotification);
-                startActivity(intent);
+                setupTemplateIntent();
             }
         });
     }
 
-    private void populateTables(TemplateDTO templateDTO){
-        // Template dto details to get list of tables
+    private void populateTables(TemplateDTO templateDTO) {
+        // onresume
+        // Template dto details to get list of tables Api List RetrieveTableDetailsDTO
 
         // populate into the scroll view
-
+        // Check box listener -> enableDelete
+        // Clickable new layout for data preview -> passing in RetrieveTableDetailsDTO
     }
 
-    private void setupTemplateIntent(){
+    // Remove button
+    private void removeTable() {
+        Button removeTable = findViewById(R.id.removeTable);
+
+        removeTable.setOnClickListener(v -> {
+            // run check on current child tables checked from view templateLinearLayout
+            // only remove checks
+            // List of TableID
+            // api delete tableDetails,Headerdetails, templatedetails Delete.
+
+        });
+    }
+
+    private void deleteButtonState(){
+        // get child view
+        // reset state
+        // loop through
+           // if 1 is enabled break set flag to true
+
+        // set button state
+    }
+
+    private void setupTemplateIntent() {
         Intent intent = getIntent();
         if (intent != null && intent.hasExtra("TEMPLATE_KEY")) {
             TemplateDTO templateDTO = intent.getParcelableExtra("TEMPLATE_KEY");
@@ -141,6 +182,7 @@ public class TemplateDetails extends AppCompatActivity {
             }
         }
     }
+
     private void displayTemplateDetails(TemplateDTO templateDTO) {
         EditText templateNameEditText = findViewById(R.id.templateName);
         EditText descriptionEditText = findViewById(R.id.templateDescription);
