bc546995fc85f8ac596c0449b94c1c80269c94ea
yuxiang322 <rlptraum@gmail.com>
2025-02-14 13:09:13 +0800

    1. Sharing Code Entity object updated with table
    ** POST -> DELETE for delete apis**

--- src/main/java/com/xiang/multipurposeTracker/repository/CustomShareInfoRepository.java
+++ src/main/java/com/xiang/multipurposeTracker/repository/CustomShareInfoRepository.java
@@ -1,7 +1,9 @@
 package com.xiang.multipurposeTracker.repository;
 
 import com.xiang.multipurposeTracker.DTO.CustomShareInfoDTO;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomShareInfoRepository {
 
     CustomShareInfoDTO getShareInfoDetails(int templateId);
