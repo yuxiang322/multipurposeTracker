a7619897add25858aada6dfafa27cec3e17989e2
yuxiang322 <rlptraum@gmail.com>
2025-02-19 04:09:13 +0800

    1. Import
    ** POST -> DELETE for delete apis**

--- src/main/java/com/xiang/multipurposeTracker/controller/ImportTemplateController.java
+++ src/main/java/com/xiang/multipurposeTracker/controller/ImportTemplateController.java
@@ -2,6 +2,7 @@ package com.xiang.multipurposeTracker.controller;
 
 import com.xiang.multipurposeTracker.DTO.ImportTemplateDTO;
 import com.xiang.multipurposeTracker.service.ImportTemplateService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ImportTemplateController {
     @Autowired
     private ImportTemplateService importTemplateService;
-
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(ImportTemplateController.class);
     @PostMapping("/template")
     public ResponseEntity<String> importTemplate(@RequestBody ImportTemplateDTO importTemplateDTO){
         try{
-
+            logger.info("Controller Useruid??: " + importTemplateDTO.getUserUID());
             String result = importTemplateService.importTemplate(importTemplateDTO);
 
             return ResponseEntity.ok(result);
